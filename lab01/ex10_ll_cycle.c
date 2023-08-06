f5b82493cb84e431210a28fe105761504aa8ab76
SpaceCruiserZao <admiralhass@hotmail.com>
2023-08-06 16:06:18 +0200

    All lab01 solved

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node* pointer1=head;
+    node* pointer2=head;
+    while (pointer1!=NULL && (*pointer1).next!=NULL){
+        pointer1=(*(*pointer1).next).next;
+        pointer2=(*pointer2).next;
+        if (pointer1==pointer2){
+            return 1;
+        }
+    }
+    return 0;
 }
