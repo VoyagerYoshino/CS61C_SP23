de6ba18c972a0fe6eb16a0b58c54ad3e5e7d9d70
SpaceCruiserZao <admiralhass@hotmail.com>
2023-08-19 23:05:14 +0200

    ReLU Test passed

--- project02/src/relu.s
+++ project02/src/relu.s
@@ -13,26 +13,19 @@
 #     this function terminates the program with error code 36
 # ==============================================================================
 relu:
-    # Prologue
-
-
+    bge x0,a1,Exceptions
+    mv t0,x0 
 loop_start:
-
-
-
-
-
-
-
-
-loop_continue:
-
-
-
+    bge t0,a1,loop_end
+    slli t1,t0,2    #offset
+    add a2,a0,t1
+    addi t0,t0,1    #t0+1
+    lw t2,0(a2)
+    bge t2,x0,loop_start
+    sw x0,0(a2)
+    j loop_start
 loop_end:
-
-
-    # Epilogue
-
-
     jr ra
+Exceptions:
+    li a0 36
+    j exit
