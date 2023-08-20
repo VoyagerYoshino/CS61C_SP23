2f9914a6d6a5d4479f751393d808ff98d70525dd
SpaceCruiserZao <admiralhass@hotmail.com>
2023-08-20 08:44:26 +0200

    All Argmax Test Passed

--- project02/src/argmax.s
+++ project02/src/argmax.s
@@ -15,16 +15,27 @@
 #     this function terminates the program with error code 36
 # =================================================================
 argmax:
-    # Prologue
-
-
+    bge x0,a1,Exceptions
+    mv t0,x0
+    mv t1,x0
+    mv,t4,x0           #argmax
+    lw t3,0(a0)     #t3 maxvalue
 loop_start:
-
-
+    bge t0,a1,loop_end
+    slli t1,t0,2    #offset
+    add t1,t1,a0    #t1 adress
+    lw t2,0(t1)     #t2 value
+    blt t3,t2,Max
 loop_continue:
-
-
+    addi t0,t0,1    #t0+1
+    j loop_start
+Max:
+    mv t3,t2
+    mv t4,t0
+    j loop_continue
 loop_end:
-    # Epilogue
-
+    mv a0,t4
     jr ra
+Exceptions:
+    addi a0,x0,36
+    j exit
