31c86e03a4efc5e68b7ad3b37b09925ed6abed8f
SpaceCruiserZao <admiralhass@hotmail.com>
2023-08-23 14:30:18 +0200

    All Test08 Passed

--- project02/src/read_matrix.s
+++ project02/src/read_matrix.s
@@ -25,18 +25,68 @@
 #     this function terminates the program with error code 29
 # ==============================================================================
 read_matrix:
-
-    # Prologue
-
-
-
-
-
-
-
-
-
-    # Epilogue
-
-
+    addi sp,sp,-28
+    sw a0,0(sp)     #store filename pointer
+    sw a1,4(sp)     #store row pointer 
+    sw a2,8(sp)     #store column pointer
+    sw ra,24(sp)
+    mv a1,x0        #Permission bits 0
+    jal fopen
+    addi t0,x0,-1
+    beq a0,t0,Exceptions_fopen  #fopen Fail?
+    sw a0,12(sp)    #store file descriptor
+    lw a1,4(sp)     #load row pointer
+    addi a2,x0,4
+    jal fread
+    addi t0,x0,4
+    bne a0,t0,Exceptions_fread  #fread Fail?
+    lw a0,12(sp)    #load file descriptor
+    lw a1,8(sp)     #load column pointer
+    addi a2,x0,4
+    jal fread
+    addi t0,x0,4
+    bne a0,t0,Exceptions_fread  #fread Fail?
+    lw t0,4(sp)     #load pointer
+    lw t1,8(sp)     #load pointer
+    lw a0,0(t0)
+    lw t0,0(t1)
+    slli a0,a0,2
+    mul a0,a0,t0    #size of memory  in Byte
+    sw a0,20(sp)
+    jal malloc
+    beq a0,x0,Exceptions_malloc    #malloc failed?
+    sw a0,16(sp)
+    
+    lw a2,20(sp)
+    lw a1,16(sp)    #malloc memory pointer
+    lw a0,12(sp)
+    jal fread
+    lw t0,20(sp)
+    bne a0,t0,Exceptions_fread  #fread Fail?
+    lw a0,12(sp)
+    jal fclose
+    bne a0,x0,Exceptions_fclose
+    lw a0,16(sp)
+    lw ra,24(sp)
+    addi sp,sp,28
     jr ra
+Exceptions_fopen:
+    addi a0,x0,27
+    lw ra,24(sp)
+    addi sp,sp,28
+    j exit
+Exceptions_fread:
+    addi a0,x0,29
+    lw ra,24(sp)
+    addi sp,sp,28
+    j exit
+Exceptions_malloc:
+    addi a0,x0,26
+    lw ra,24(sp)
+    addi sp,sp,28
+    j exit
+Exceptions_fclose:
+    addi a0,x0,28
+    lw ra,24(sp)
+    addi sp,sp,28
+    j exit
--- project02/src/write_matrix.s
+++ project02/src/write_matrix.s
@@ -23,18 +23,52 @@
 #     this function terminates the program with error code 30
 # ==============================================================================
 write_matrix:
-
-    # Prologue
-
-
-
-
-
-
-
-
-
-    # Epilogue
-
-
+    addi sp,sp,-24
+    sw ra,0(sp)
+    sw a1,4(sp)     #pointer of matrix start
+    sw a2,8(sp)     #number of rows
+    sw a3,12(sp)    #number of columns
+    
+    addi a1,x0,1    #Permission bit for write
+    jal fopen
+    addi t0,x0,-1
+    beq a0,t0,Exceptions_fopen  #fopen fail?
+    sw a0,16(sp)    #File Description
+    addi a1,sp,8
+    addi a2,x0,2    #number of element
+    addi a3,x0,4
+    jal fwrite
+    addi t0,x0,2
+    bne a0,t0,Exceptions_fwrite     #fwrite fail?
+    lw a0,16(sp)    #File Descriptor
+    lw a1,4(sp)
+    lw a2,8(sp)
+    lw a3,12(sp)    
+    mul a2,a2,a3    #number of elements
+    sw a2,20(sp)    #number of element
+    addi a3,x0,4    #4 Byte each elment
+    jal fwrite
+    lw t0,20(sp)
+    bne a0,t0,Exceptions_fwrite #fwrite fail?
+    lw a0,16(sp)
+    jal fclose
+    bne a0,x0,Exceptions_fclose
+    lw ra,0(sp)
+    addi sp,sp,24
     jr ra
+    
+Exceptions_fopen:
+    lw ra,0(sp)
+    addi sp,sp,24
+    addi a0,x0,27
+    j exit
+Exceptions_fwrite:
+    lw ra,0(sp)
+    addi sp,sp,24
+    addi a0,x0,30
+    j exit    
+Exceptions_fclose:
+    lw ra,0(sp)
+    addi sp,sp,24
+    addi a0,x0,28
+    j exit        
