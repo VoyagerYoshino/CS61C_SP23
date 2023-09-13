fe32837a22a17b44d7b3aa93cfbc5c56297487e4
SpaceCruiserZao <admiralhass@hotmail.com>
2023-09-14 00:38:34 +0200

    All Vector Test Passed

--- lab07/ex2.c
+++ lab07/ex2.c
@@ -3,19 +3,39 @@
 void v_add_naive(double* x, double* y, double* z) {
     #pragma omp parallel
     {
-        for(int i=0; i<ARRAY_SIZE; i++)
-            z[i] = x[i] + y[i];
+    for(int i=0; i<ARRAY_SIZE; i++)
+        z[i] = x[i] + y[i];
     }
 }
 
 // Adjacent Method
 void v_add_optimized_adjacent(double* x, double* y, double* z) {
     // TODO: Implement this function
+     #pragma omp parallel
+     {
+        int thread_num=omp_get_num_threads();
+        int thread_id=omp_get_thread_num();
+
+        for(int i=thread_id;i<ARRAY_SIZE;i+=thread_num){
+            z[i] = x[i] + y[i];
+        }
+     }
     // Do NOT use the `for` directive here!
 }
 
 // Chunks Method
 void v_add_optimized_chunks(double* x, double* y, double* z) {
-    // TODO: Implement this function
+    #pragma omp parallel
+    {
+        // TODO: Implement this function
+        int thread_num=omp_get_num_threads();
+        int thread_id=omp_get_thread_num();
+        int chunk=ARRAY_SIZE/thread_num;
+        int start=thread_id*chunk;
+        int end=(thread_id==thread_num-1)?ARRAY_SIZE:start+chunk;
+        for(int i=start;i<end;i++){
+            z[i]=x[i]+y[i];
+        }
+    }
     // Do NOT use the `for` directive here!
 }
--- lab07/openmp_example.c
+++ lab07/openmp_example.c
@@ -2,6 +2,8 @@
 #include <omp.h>
 
 int main() {
+    int num_threads=2;
+    omp_set_num_threads(num_threads);
     #pragma omp parallel
     {
         int thread_ID = omp_get_thread_num();
