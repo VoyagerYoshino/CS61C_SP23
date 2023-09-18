1e5ada8d85e02dcce5dcee9b0c3615c2b81135ea
SpaceCruiserZao <admiralhass@hotmail.com>
2023-09-18 17:12:50 +0200

    Create an Imgfolder For Readme

--- project04/src/compute_naive.c
+++ project04/src/compute_naive.c
@@ -1,18 +1,43 @@
 #include "compute.h"
 
 // Computes the dot product of vec1 and vec2, both of size n
-int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
+int flip_dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
   // TODO: implement dot product of vec1 and vec2, both of size n
-
-  return -1;
+  int result=0;
+  for(uint32_t i=0;i<n;i++){
+    result+=vec1[i]*vec2[n-i-1];
+  }
+  return result;
 }
 
 // Computes the convolution of two matrices
 int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
-  // output_matrix
+  uint32_t row_output=a_matrix->rows-b_matrix->rows+1;
+  uint32_t col_output=a_matrix->cols-b_matrix->cols+1;
+  matrix_t *result=(matrix_t*)malloc(sizeof(matrix_t));
+  if (result==NULL){
+    return -1;
+  }
+  result->rows=row_output;
+  result->cols=col_output;
+  result->data=(int32_t*)malloc(row_output*col_output*sizeof(int32_t));
+  if (result->data==NULL){
+    return -1;
+  }
+  uint32_t index=0;
 
-  return -1;
+  for(int i=0;i<row_output;i++){
+    for(int j=0;j<col_output;j++){
+          for(int a_index=0;a_index<a_matrix->cols*b_matrix->rows;a_index+=a_matrix->cols){
+            result->data[index]+=flip_dot(b_matrix->cols,(a_matrix->data)+sizeof(int32_t)*a_index,b_matrix->data);
+          }
+          index+=1;
+    }
+  }
+  // output_matrix
+  *output_matrix=result;
+  return 0;
 }
 
 // Executes a task
